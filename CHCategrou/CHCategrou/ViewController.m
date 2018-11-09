d4a4bf568b2abab7f4cfd32ca911ec3a4fcb1b29
15036025297 <15036025297@163.com>
2018-11-09 13:05:15 +0800

    测试

---
+++ CHCategrou/CHCategrou/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  CHCategrou
+//
+//  Created by chenhui on 2018/11/9.
+//  Copyright © 2018年 chenhui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
